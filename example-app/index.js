aa4da321a53d721f9973731d20fb4652cf6783fe
David Feroli <daferoli@gmail.com>
2020-04-08 11:14:13 -0400

    created extremely simple node app to deploy and monitor

---
+++ example-app/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const promClient = require('prom-client');
+
+const app = express();
+
+promClient.collectDefaultMetrics();
+
+app.get('/test', (req, res) => {
+  res.send('Hello from the test endpoint');
+});
+
+app.get('/metrics', (req, res) => {
+  res.send(promClient.register.metrics());
+});
+
+app.listen(8080, () => {
+  console.log('app started on port 8080');
+});
